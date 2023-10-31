1a8f01c15292f62912b215d3ebdb7d9e329d4778
Gabriela-Daniela Laurus <gabriela.laurus@student.upt.ro>
2023-10-31 12:18:58 +0200

    single-post-page-done

--- src/Post.js
+++ src/Post.js
@@ -2,15 +2,15 @@ import {format} from "date-fns";
 import {Link} from "react-router-dom";
 
 
-export default function Post({title, summary, cover, content, createdAt, author}){
+export default function Post({_id, title, summary, cover, content, createdAt, author}){
     return(<div className="post">
         <div  className="image">
-            <Link to={'/post/id'}>
+            <Link to={`/post/${_id}`}>
                 <img src={'http://localhost:4000/'+cover}/>
             </Link>
         </div>
         <div className="texts">
-            <Link to={'/post/id'}>
+            <Link to={`/post/${_id}`}>
                 <h2>{title}</h2>
             </Link>
             <p className="info">
